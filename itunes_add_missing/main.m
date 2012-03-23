07b9cd625c6b56ce0dd87c3d322f7bdadc2ee7c9
Jacob Vosmaer <contact@jacobvosmaer.nl>
2012-03-23 17:54:33 +0100

    moved inode hashing into a function; refactored two variable names

--- itunes_add_missing/main.m
+++ itunes_add_missing/main.m
@@ -9,6 +9,8 @@
 #import <Foundation/Foundation.h>
 #import "iTunes.h"
 
+NSString *hashForPath(NSString *path, NSFileManager *fM);
+
 int main (int argc, const char * argv[])
 {
     
@@ -22,31 +24,40 @@ int main (int argc, const char * argv[])
         
         NSArray *iTunesFileTrackURLs = [[libraryPlaylist fileTracks] arrayByApplyingSelector:@selector(location)];
         NSUInteger numberOfItunesTracks = [iTunesFileTrackURLs count];
-        NSMutableArray *iTunesInodes = [[NSMutableArray alloc] initWithCapacity:numberOfItunesTracks];
+        NSMutableArray *iTunesHashes = [[NSMutableArray alloc] initWithCapacity:numberOfItunesTracks];
         NSFileManager *fileManager = [NSFileManager defaultManager];
         for (NSURL *anURL in iTunesFileTrackURLs) {
-            NSDictionary *fileProperties = [fileManager attributesOfItemAtPath:[anURL path] error:nil];
-            NSNumber *inode = [fileProperties valueForKey:NSFileSystemFileNumber];
-            NSNumber *devNumber = [fileProperties valueForKey:NSFileSystemNumber];
-            if (devNumber && inode) {
-                [iTunesInodes addObject:[NSString stringWithFormat:@"%d+%d", devNumber,inode]];
+            NSString *hash = hashForPath([anURL path], fileManager);
+            if (hash) {
+                [iTunesHashes addObject:hash];
             }
         }
         
         NSString *inputString = [[NSString alloc] initWithData:[[NSFileHandle fileHandleWithStandardInput] readDataToEndOfFile]encoding:NSUTF8StringEncoding];
         NSArray *inputItems = [inputString componentsSeparatedByString:@"\n"];
-        NSMutableArray *newURLS = [[NSMutableArray alloc] init];
+        NSMutableArray *newURLs = [[NSMutableArray alloc] init];
         for (NSString *aString in inputItems) {
-            NSDictionary *fileProperties = [fileManager attributesOfItemAtPath:aString error:nil];
-            NSNumber *inode = [fileProperties valueForKey:NSFileSystemFileNumber];
-            NSNumber *devNumber = [fileProperties valueForKey:NSFileSystemNumber];
-            if (inode && ![iTunesInodes containsObject:[NSString stringWithFormat:@"%d+%d", devNumber,inode]]) {
-                [newURLS addObject:[[NSURL alloc] initFileURLWithPath:aString]];
+            NSString *hash = hashForPath(aString, fileManager);
+            if (![iTunesHashes containsObject:hash]) {
+                [newURLs addObject:[[NSURL alloc] initFileURLWithPath:aString]];
             }
         }
-        printf("Adding %lu new items to iTunes library.\n", (unsigned long int)[newURLS count]);
-        [iTunes add:newURLS to:libraryPlaylist];
+        printf("Adding %lu new items to iTunes library.\n", (unsigned long int)[newURLs count]);
+        [iTunes add:newURLs to:libraryPlaylist];
     }
     return 0;
 }
 
+NSString *hashForPath(NSString *path, NSFileManager *fM)
+{
+    @autoreleasepool {
+        NSString *result = nil;
+        NSDictionary *fileProperties = [fM attributesOfItemAtPath:path error:nil];
+        NSNumber *inode = [fileProperties valueForKey:NSFileSystemFileNumber];
+        NSNumber *devNumber = [fileProperties valueForKey:NSFileSystemNumber];
+        if (devNumber && inode) {
+            result = [[NSString alloc] initWithFormat:@"%d+%d", devNumber,inode];
+        }
+        return result;
+    }
+}

4cf2482f8b08d6060fd1291c5b1e6899d4c419b9
Jacob Vosmaer <contact@jacobvosmaer.nl>
2012-04-18 12:19:54 +0200

    accept - to read paths from stdin

--- make_itunes_playlist/main.m
+++ make_itunes_playlist/main.m
@@ -18,7 +18,7 @@ int main (int argc, const char * argv[])
     @autoreleasepool {
         
         if ((argc < 2)) {
-            printf("Usage: %s playlist_name file1 file2 ...\n", argv[0]);
+            printf("Usage: %s playlist_name (file1 [file2 ...] | - )\n", argv[0]);
             return 2;
         }
         
@@ -32,14 +32,25 @@ int main (int argc, const char * argv[])
         NSString *playlistName = [[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding] stringByAppendingFormat:@" (%@)", [[NSDate date] descriptionWithLocale:[NSLocale currentLocale]]];
         iTunesUserPlaylist *thePlaylist =[[[iTunes classForScriptingClass:ITUNES_USER_PLAYLIST_SCRIPTINGCLASS] alloc] initWithProperties:[NSDictionary dictionaryWithObject:playlistName forKey:ITUNES_PLAYLIST_NAME_PROPERTY]];
         [userPlaylists addObject:thePlaylist];
+
+        NSMutableArray *arguments = [NSMutableArray arrayWithCapacity:argc];
+        for (NSUInteger i=2; i<argc; i++) {
+            [arguments addObject: [NSString stringWithCString:argv[i] encoding:NSUTF8StringEncoding]];
+        }
         
-        NSMutableArray *listOfTracks = [NSMutableArray arrayWithCapacity:argc];
+        NSArray *listOfPaths;
+        if ([arguments count] == 1 && ([[arguments objectAtIndex:0] isEqualToString:@"-"])) {
+            NSString *inputString = [[[NSString alloc] initWithData:[[NSFileHandle fileHandleWithStandardInput] readDataToEndOfFile]encoding:NSUTF8StringEncoding] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            listOfPaths = [inputString componentsSeparatedByString:@"\n"];
+        } else {
+            listOfPaths = arguments;
+        }
+        
+        NSMutableArray *listOfTracks = [[NSMutableArray alloc] init];
         NSFileManager *filemanager = [NSFileManager defaultManager];
-        NSUInteger i;
-        for (i=2; i<argc; i++) {
-            NSString *currentArgument = [NSString stringWithCString:argv[i] encoding:NSUTF8StringEncoding];
-            if ([filemanager fileExistsAtPath:currentArgument]) {
-                [listOfTracks addObject:[NSURL fileURLWithPath:currentArgument]];
+        for (NSString *path in listOfPaths) {
+            if ([filemanager fileExistsAtPath:path]) {
+                [listOfTracks addObject:[NSURL fileURLWithPath:path]];
             }
         }
         [iTunes add:listOfTracks to:thePlaylist];
